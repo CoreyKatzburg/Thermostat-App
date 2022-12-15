cb8eeee5eb2d251614d4a15a9011e2447e2e7094
Corey Katzburg <64795827+CoreyKatzburg@users.noreply.github.com>
2022-12-15 16:50:23 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function loginPressed(){
-   window.location='second.html';
-   clearInterval(interval);
-}
-
-function homeBtnPressed(){
-   window.location='main.html';
-   clearInterval(Interval);
-}
